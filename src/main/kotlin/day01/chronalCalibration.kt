cfbbcaf3a656f46a3a42e5b1b0b23f4aa0188025
RoryCollins <rory.collins@hotmail.co.uk>
2022-11-18 15:28:22 +0000

    Fix linting

--- src/main/kotlin/day01/chronalCalibration.kt
+++ src/main/kotlin/day01/chronalCalibration.kt
@@ -2,14 +2,13 @@ package day01
 
 import java.io.File
 
-val numbers = File("src/main/kotlin/day01/input.txt")
+val numbers: List<Int> = File("src/main/kotlin/day01/input.txt")
     .readLines()
     .map { it.toInt() }
 
 fun solve(): Int{
     val frequencies = mutableSetOf<Int>()
-    var index = 0
-    var frequency: Int = 0
+    var frequency = 0
 
     while (true) {
         for(next in numbers){
