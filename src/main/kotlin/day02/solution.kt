0e40c804a1017ef68e744c8ee5648c5c5aab031b
Rory Collins <rory.collins@hotmail.co.uk>
2022-11-21 09:30:24 +0000

    WIP day 2 part 2

--- src/main/kotlin/day02/solution.kt
+++ src/main/kotlin/day02/solution.kt
@@ -4,14 +4,25 @@ import java.io.File
 
 val lines = File("src/main/kotlin/day02/input.txt")
     .readLines()
-    .map{ containsNRepeatedLetter(3, it) to containsNRepeatedLetter(2, it) }
 
 fun containsNRepeatedLetter(n: Int, s: String) : Int{
     return if (s.toSet().any { letter -> s.count { it == letter } == n }) 1
         else 0
 }
 
+fun wordCorrelation(s1: String, s2: String): String{
+    return s1.indices.filter { s1[it] == s2[it]}.joinToString()
+}
+
 fun main(){
-    lines.map { println(it) }
-    println("Part One: ${lines.sumOf { it.first } * lines.sumOf { it.second }}")
+    val repetitionPairs = lines
+        .map{ containsNRepeatedLetter(2, it) to containsNRepeatedLetter(2, it) }
+
+    val indices = lines.indices
+    for (i in indices){
+         (i .. indices.last).max{ wordCorrelation(lines[i], lines[it]).length }
+    }
+    println("Part One: ${repetitionPairs.sumOf { it.first } * repetitionPairs.sumOf { it.second }}")
+    println("Part Two: ${lines.maxBy { line -> line.maxBy { wordCorrelation(it, ).length } }}")
+
 }
